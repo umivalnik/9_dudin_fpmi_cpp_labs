0b259c70233df754e196d00a5f5aa74971db4800
Denis Dudin <dudindenis29@gmail.com>
2025-11-05 10:04:42 +0300

    Update task26.cpp

--- semester_1/lab3_pointers_and_arrays/task26.cpp
+++ semester_1/lab3_pointers_and_arrays/task26.cpp
@@ -76,16 +76,8 @@ int main() {
     }
 
     double umn = 1.0;
-    int start = std::min(maxindex, minindex);
-    int end = std::max(maxindex, minindex);
-    bool elements_between = false;
-
-    for (int i = start + 1; i < end; i++) {
+    for (int i = newindex + 1; i < maxindex; i++) {
         umn *= arr[i];
-        elements_between = true;
-    }
-    if (!elements_between) {
-        umn = 0.0;
     }
 
     std::cout << "произведение: " << umn << std::endl;
