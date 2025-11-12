aeb23687de9a4674d83667c53b83ef8d64b58644
Denis Dudin <dudindenis29@gmail.com>
2025-11-12 09:59:17 +0300

    Update task26.cpp

--- semester_1/lab3_pointers_and_arrays/task26.cpp
+++ semester_1/lab3_pointers_and_arrays/task26.cpp
@@ -3,46 +3,62 @@
 #include <algorithm>
 #include <random>
 
-const int max1 = 100;
-
 void imputingwithcheck(int& n) {
-
     setlocale(LC_ALL, "Russian");
 
-    std::cout << "введите количество элементов менее " << max1 << "): ";
+    std::cout << "Введите количество элементов: ";
     std::cin >> n;
 
-    if (n <= 0 || n > max1) {
-        std::cout << "ошибка размера" << std::endl;
+    if (std::cin.fail() || n <= 0) {
+        std::cout << "Ошибка: некорректное количество элементов.\n";
         std::exit(1);
     }
 }
 
 int main() {
-        
     setlocale(LC_ALL, "Russian");
 
-    double arr[max1];
     int n;
     int choice;
 
     imputingwithcheck(n);
 
-    std::cout << "выберите способ заполнения:" << std::endl;
-    std::cout << "1 - ввод с клавиатуры " << std::endl;
-    std::cout << "2 - случайное заполнение " << std::endl;
+    double* arr = new double[n];
+
+    std::cout << "Выберите способ заполнения:\n";
+    std::cout << "1 - ввод с клавиатуры\n";
+    std::cout << "2 - случайное заполнение\n";
     std::cin >> choice;
 
+    if (std::cin.fail()) {
+        std::cout << "Ошибка: введено не число.\n";
+        return 1;
+    }
+
     if (choice == 1) {
-        std::cout << "введите элементы массива: ";
+        std::cout << "Введите элементы массива:\n";
         for (int i = 0; i < n; i++) {
             std::cin >> arr[i];
+            if (std::cin.fail()) {
+                std::cout << "Ошибка: введено не число.\n";
+                return 1;
+            }
         }
     }
     else if (choice == 2) {
         double a, b;
-        std::cout << "введите границы [a, b]: ";
+        std::cout << "Введите границы [a, b]: ";
         std::cin >> a >> b;
+
+        if (std::cin.fail()) {
+            std::cout << "Ошибка: введены некорректные значения.\n";
+            return 1;
+        }
+
+        if (a > b) {
+            std::swap(a, b);
+            std::cout << "Границы перепутаны. Меняю местами: a = " << a << ", b = " << b << std::endl;
+        }
 
         std::random_device rd;
         std::mt19937 gen(rd());
@@ -53,11 +69,11 @@ int main() {
         }
     }
     else {
-        std::cout << "ошибка" << std::endl;
+        std::cout << "Ошибка: нужно выбрать 1 или 2.\n";
         return 1;
     }
 
-    std::cout << "исходный массив: ";
+    std::cout << "Исходный массив: ";
     for (int i = 0; i < n; i++) {
         std::cout << arr[i] << " ";
     }
@@ -76,72 +92,78 @@ int main() {
     }
 
     double umn = 1.0;
-    for (int i = minindex + 1; i < maxindex; i++) {
+    for (int i = std::min(minindex, maxindex) + 1; i < std::max(minindex, maxindex); i++) {
         umn *= arr[i];
     }
 
-    std::cout << "произведение: " << umn << std::endl;
+    std::cout << "Произведение элементов между min и max: " << umn << std::endl;
 
     int N;
-    std::cout << "введите количество элементов для удаления: ";
+    std::cout << "Введите количество элементов для удаления: ";
     std::cin >> N;
 
-    if (N > 0 && N <= n) {
-        double temp[max1];
-        int indices[max1];
-
-        for (int i = 0; i < n; i++) {
-            temp[i] = arr[i];
-            indices[i] = i;
-        }
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (temp[j] > temp[j + 1]) {
-                    std::swap(temp[j], temp[j + 1]);
-                    std::swap(indices[j], indices[j + 1]);
-                }
-            }
-        }
-
-        bool to_remove[max1] = { false };
-        for (int i = 0; i < N; i++) {
-            to_remove[indices[i]] = true;
-        }
-
-        int new_index = 0;
-        for (int i = 0; i < n; i++) {
-            if (!to_remove[i]) {
-                arr[new_index] = arr[i];
-                new_index++;
-            }
-        }
-
-        n = new_index;
-
-        std::cout << "после удаления " << N << " наименьших элементов: ";
-        for (int i = 0; i < n; i++) {
-            std::cout << arr[i] << " ";
-        }
-        std::cout << std::endl;
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (std::fabs(arr[j]) > std::fabs(arr[j + 1])) {
-                    std::swap(arr[j], arr[j + 1]);
-                }
-            }
-        }
-
-        std::cout << "после сортировки: ";
-        for (int i = 0; i < n; i++) {
-            std::cout << arr[i] << " ";
-        }
-        std::cout << std::endl;
+    if (std::cin.fail() || N <= 0 || N > n) {
+        std::cout << "Ошибка: некорректное значение N.\n";
+        return 1;
     }
-    else {
-        std::cout << "ошибочное N" << std::endl;
+
+    double* temp = new double[n];
+    int* indices = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        temp[i] = arr[i];
+        indices[i] = i;
     }
+
+    // сортировка по возрастанию
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (temp[j] > temp[j + 1]) {
+                std::swap(temp[j], temp[j + 1]);
+                std::swap(indices[j], indices[j + 1]);
+            }
+        }
+    }
+
+    bool* to_remove = new bool[n];
+    for (int i = 0; i < n; i++) {
+        to_remove[i] = false;
+    }
+
+    for (int i = 0; i < N; i++) {
+        to_remove[indices[i]] = true;
+    }
+
+    int new_index = 0;
+    for (int i = 0; i < n; i++) {
+        if (!to_remove[i]) {
+            arr[new_index] = arr[i];
+            new_index++;
+        }
+    }
+
+    n = new_index;
+
+    std::cout << "После удаления " << N << " наименьших элементов: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    // сортировка по модулю
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (std::fabs(arr[j]) > std::fabs(arr[j + 1])) {
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+
+    std::cout << "После сортировки по модулю: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
 }
