ce78063e568ac15dc97839320ea5bc0a41b1bc91
Denis Dudin <dudindenis29@gmail.com>
2025-12-10 12:32:16 +0300

    Update task8.cpp

--- semester_1/lab2_integer_arithmetics/task8.cpp
+++ semester_1/lab2_integer_arithmetics/task8.cpp
@@ -2,23 +2,27 @@
 using namespace std;
 
 int main() {
+    setlocale(LC_ALL, "ru");
     int a, b;
     cout << "введите a, затем b ";
     cin >> a >> b;
-    for(int n = a; n <= b; n++) {
+    if (a > b) {
+        swap(a, b);
+    }
+    for (int n = a; n <= b; n++) {
         if (n < 2) continue;
         int i;
-        for (i = 2; i <= n-1; i++) {
+        for (i = 2; i <= n - 1; i++) {
             if (n % i == 0) {
                 break;
-                
+
             }
         }
         if (i * i > n) {
             cout << n << " ";
         }
-    }   
-    
+    }
+
     return 0;
-    
+
 }
