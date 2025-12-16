c7981ac16ae8fa1835fa65920b974893eb1b2f38
Denis Dudin <dudindenis29@gmail.com>
2025-12-17 00:54:49 +0300

    Update vector_impl.cpp

--- semester_1/lab7_class_vector/vector/vector_impl.cpp
+++ semester_1/lab7_class_vector/vector/vector_impl.cpp
@@ -1,3 +1,34 @@
 #include "vector_impl.h"
+#pragma once
+#include <iostream>
+class Vector {
+private:
+    int* data;
+    size_t size;
+    size_t capacity;
 
-// your code goes here
+public:
+    size_t Size() const;
+    size_t Capacity() const;
+    int& operator[](size_t index);
+    const int& operator[](size_t index) const;
+    int& At(size_t index);
+    const int& At(size_t index) const;
+
+    Vector();
+    Vector(size_t n);
+    Vector(const Vector& other);
+    Vector(std::initializer_list<int> list);
+    ~Vector();
+
+    Vector& operator=(const Vector& other);
+    void Swap(Vector& other);
+    void PushBack(int a);
+    void PopBack();
+    void Clear();
+    void Reserve(size_t n);
+
+};
+std::ostream& operator<<(std::ostream& out, const Vector& vec);
+
+
